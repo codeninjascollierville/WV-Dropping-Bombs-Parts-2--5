0084988620a1a38f7c0795e667773aad2bc4ea86
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2022-11-15 18:02:50 -0600

    Bomb Drop WV P2
    Lamp oil
    11/15/22

---
+++ Assets/Scripts/RocketAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Animator")]
+    public Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("HAxis", horizontal);
+    }
+}
