2e1db12c93d5562e9ba3dfcfc7b55084e0a9e1c8
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2022-11-29 17:54:06 -0600

    Dropping Bombs P3 #2
    11/29/22

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,23 +5,64 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    private Vector2 screenBounds;
+    public GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
 
     void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        player = playerPrefab;
     }
     // Start is called before the first frame update
     void Start()
     {
         spawner.active = false;
+        title.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (!gameStarted)
         {
-            spawner.active = true;
+            if (Input.anyKeyDown)
+            {
+                ResetGame();
+            }
         }
+        else
+        {
+            if (!player)
+            {
+                OnPlayerKilled();
+            }
+        }
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y) - 12 || !gameStarted)
+            {
+                Destroy(bombObject);
+            }
+        }
+        void OnPlayerKilled()
+        {
+            spawner.active = false;
+            gameStarted = false;
+
+            splash.SetActive(false);
+        }
+    }
+    void ResetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+        player = Instantiate(playerPrefab, new Vector3(0, 0, 0), playerPrefab.transform.rotation);
+        gameStarted = true;
     }
 }
